7fcede73b816c87f8b49cf27a6c9f19061d10a0f
Mira Vogt <miravogt@gmail.com>
2020-09-15 15:25:06 +0200

    integration of chart.js on dashboard

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router  = express.Router();
 const {isLoggedIn} = require('./middlewares');
 const Doctor = require('../models/Doctor');
 const Patient = require('../models/Patient');
-const Treatment = require('../models/Treatment')
+const Treatment = require('../models/Treatment');
+
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -21,85 +23,25 @@ router.get('/overview', isLoggedIn(), (req, res, next) => {
   .catch(err => next(err));
 });
 
-router.get('/patient/:id', (req, res, next) => {
-  console.log('route called');
+router.get('/patient/:id', isLoggedIn(), (req, res, next) => {
   Patient.findById(req.params.id)
   .populate('treatments')
   .then(patient => {
-    console.log(patient);
     res.render('doctor/patientDashboard', {
-      patient: patient,
+      patient: patient
     });
   })
   .catch(err => next(err));
 }); 
 
-// router.get('/addPatient', (req, res, next) => {
-//   Patient.create({
-//     patientId: 111111111,
-//   healthInsuranceId: 22222222,
-//   firstName: 'Marlena',
-//   lastName: 'Koslowski',
-//   birthDate: new Date(1995, 4, 1),
-//   contact: {
-//     email: "m.k@gmail.com",
-//     telephone: 004912345678
-//   },
-//   diagnosis: 'Diabetes',
-//   allergies: 'Gluten',
-//   alertLevel: 'Low',
-//   treatments: [],
-//   bloodPressureData: [{
-//     isMeasured: true,
-//     min: {
-//       systolic: 90,
-//       diastolic: 80
-//     },
-//     max: {
-//       systolic: 180,
-//       diastolic: 170
-//     },
-//     target: {
-//       systolic: 150,
-//       diastolic: 100
-//     },
-//     date: new Date(2020, 05, 11),
-//     value: [{
-//             systolic: 100,
-//             diastolic: 80
-//         }],
-//     comment: 'blablablubb'
-//   }],
-//   heartFrequencyData: [{
-//     isMeasured: false,
-//     min: '',
-//     max: '',
-//     target: '',
-//     date: '',
-//     value: '',
-//     comment:''
-//   }],
-//   bloodSugarData: [{
-//     isMeasured: true,
-//     min: 80,
-//     max: 120,
-//     target: 100,
-//     date: new Date(2020, 8, 12),
-//     value: 130,
-//     comment: 'blood sugar too high'
-//   }],
-//   assignedDoctor: req.session.user._id,
-//   lastUpdate: new Date(2020, 8, 12)
-// })
-// .then(patient => {
-//   Doctor.findByIdAndUpdate(req.session.user._id, { $push: {patients: patient._id}})
-//   .then(doctor => {
-//     console.log(doctor);
-//   })
-//   .catch(err => next(err));
+router.get('/api/patient/:id', isLoggedIn(), (req, res, next) => {
+  Patient.findById(req.params.id)
+  .populate('treatments')
+  .then(patient => {
+    res.json(patient);
+  })
+  .catch(err => next(err));
+}); 
 
-// })
-// .catch(err => next(err));
-//   })
 
 module.exports = router;
