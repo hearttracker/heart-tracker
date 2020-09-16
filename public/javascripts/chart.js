3fbdf8a1bf598a24d3de4dc312356a041805abc5
Mira Vogt <miravogt@gmail.com>
2020-09-16 12:41:39 +0200

    add script for dynamic views

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,9 +1,9 @@
 
 
 const bloodSugarChart = (data) => {
-  const dates = data.map(value => (value.date).toString());
+  const dates = data.slice(0,10).map(value => (value.date).toString());
   console.log({ dates })
-  const measurement = data.map(value => value.value);
+  const measurement = data.slice(0,10).map(value => value.value);
   console.log({ measurement });
 
   const ctx = document.getElementById('bloodSugarChart').getContext('2d');
@@ -16,7 +16,8 @@ const bloodSugarChart = (data) => {
           label: 'Blood Sugar Values',
           fill: false,
           borderColor: 'rgb(224, 189, 13)',
-          data: measurement
+          data: measurement,
+          yAxisId: 'first-y-axis'
         }
       ]
     }
@@ -24,12 +25,12 @@ const bloodSugarChart = (data) => {
 };
 
 const bloodPressureChart = (bloodPressureData, heartFrequencyData) => {
-  const dates = bloodPressureData.map(value => value.date);
+  const dates = bloodPressureData.slice(0,10).map(value => value.date);
   console.log({ dates })
-  const systolic = bloodPressureData.map(value => value.systolic);
-  const diastolic = bloodPressureData.map(value => value.diastolic);
+  const systolic = bloodPressureData.slice(0,10).map(value => value.systolic);
+  const diastolic = bloodPressureData.slice(0,10).map(value => value.diastolic);
   console.log({ systolic, diastolic });
-  const heartFrequency = heartFrequencyData.map(value => value.value);
+  const heartFrequency = heartFrequencyData.slice(0,10).map(value => value.value);
 
   const ctx = document.getElementById('heartRateGraph').getContext('2d');
 
@@ -42,22 +43,56 @@ const bloodPressureChart = (bloodPressureData, heartFrequencyData) => {
           label: 'Systolic',
           borderColor: 'rgb(247, 24, 2)',
           data: systolic,
-          fill: false
+          fill: false,
+          yAxisId: 'first-y-axis'
         },
         {
           label: 'Diastolic',
           borderColor: 'rgb(29, 2, 237)',
           data: diastolic,
-          fill: false
+          fill: false,
+          yAxisId: 'first-y-axis'
         },
         {
           label: 'Heart Frequency',
           borderColor: 'rgb(2, 247, 96)',
           data: heartFrequency,
-          fill: false
+          fill: false,
+          yAxisId: 'second-y-axis'
         }
       ]
-    }
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          ticks: {
+              display: false
+          }
+      }],
+          yAxes: [{
+              id: 'first-y-axis',
+              type: 'linear',
+              position: 'left',
+              ticks: {
+                min: 40,
+                max: 200,
+                display: false
+              },
+              scaleShowLabels : false
+
+          }, {
+              id: 'second-y-axis',
+              type: 'linear',
+              position: 'right',
+              ticks: {
+                min: 40,
+                max: 200,
+                display: false
+              }
+
+          }]
+      }
+  }
   });
 };
 
@@ -67,7 +102,54 @@ fetch(`http://localhost:3000/api/patient/${patientId}`)
 .then(response => response.json())
 .then(data => {
   console.log(data);
-  bloodSugarChart(data.bloodSugarData.values);
-  bloodPressureChart(data.bloodPressureData.values, data.heartFrequencyData.values);
+  // if(data.bloodSugarData.isMeasured) {
+    bloodSugarChart(data.bloodSugarData.values);
+  // };
+  // if(data.bloodPressureData.isMeasured && data.heartFrequencyData.isMeasured) {
+    bloodPressureChart(data.bloodPressureData.values, data.heartFrequencyData.values)
+  // };
 })
 
+
+
+const bloodPressure = document.getElementById('heartRateContainer');
+const bloodSugar = document.getElementById('bloodSugarContainer');
+
+bloodPressure.addEventListener("click", function() {
+  // if(!bloodPressure.classList.contains('active')) {
+  bloodPressure.classList.toggle('active');
+  bloodSugar.classList.toggle('active');
+
+  let bloodPressureParent = bloodPressure.parentElement;
+  bloodPressureParent.style.width = '60vw';
+  bloodPressure.style.width = '60vw';
+  bloodPressure.style.flexDirection = 'row';
+
+  let bloodSugarParent = bloodSugar.parentElement;
+  bloodSugarParent.style.width = '20vw';
+  bloodSugar.style.width = '20vw';
+  bloodSugar.style.flexDirection = 'column';
+
+  console.log(bloodSugarParent);
+// }
+});
+
+bloodSugar.addEventListener("click", function() {
+  console.log('clicked');
+  // if(!bloodSugar.classList.contains('active')) {
+  bloodSugar.classList.toggle('active');
+  bloodPressure.classList.toggle('active');
+  let bloodSugarParent = bloodSugar.parentElement;
+  bloodSugarParent.style.width = '60vw';
+  bloodSugar.style.width = '60vw';
+  bloodSugar.style.flexDirection = 'row';
+  console.log(bloodSugarParent);
+
+  let bloodPressureParent = bloodPressure.parentElement;
+  bloodPressureParent.style.width = '20vw';
+  bloodPressure.style.width = '20vw';
+  bloodPressure.style.flexDirection = 'column';
+
+  console.log(bloodPressureParent);
+// }
+});
