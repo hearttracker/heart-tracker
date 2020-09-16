4368215d5865c278de19c07d5c3374b65dfe5cec
Mira Vogt <miravogt@gmail.com>
2020-09-16 20:04:51 +0200

    changed dashboard appearance

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -33,7 +33,7 @@ const bloodPressureChart = (bloodPressureData, heartFrequencyData) => {
   console.log({ systolic, diastolic });
   const heartFrequency = heartFrequencyData.slice(0,10).map(value => value.value);
 
-  const ctx = document.getElementById('heartRateGraph').getContext('2d');
+  const ctx = document.getElementById('bloodPressureGraph').getContext('2d');
 
   new Chart(ctx, {
     type: 'line',
@@ -114,45 +114,60 @@ fetch(`http://localhost:3000/api/patient/${patientId}`)
 
 
 
-const bloodPressure = document.getElementById('heartRateContainer');
-const bloodSugar = document.getElementById('bloodSugarContainer');
+// const bloodPressure = document.getElementById('heartRateContainer');
+// const bloodSugar = document.getElementById('bloodSugarContainer');
 
-bloodPressure.addEventListener("click", function() {
-  if(!bloodPressure.classList.contains('active')) {
-  bloodPressure.classList.toggle('active');
-  bloodSugar.classList.toggle('active');
+// bloodPressure.addEventListener("click", function() {
+//   if(!bloodPressure.classList.contains('active')) {
+//   bloodPressure.classList.toggle('active');
+//   bloodSugar.classList.toggle('active');
 
-  let bloodPressureParent = bloodPressure.parentElement;
-  bloodPressureParent.style.width = '60vw';
-  bloodPressure.style.width = '60vw';
-  bloodPressure.style.flexDirection = 'row';
+//   let bloodPressureParent = bloodPressure.parentElement;
+//   bloodPressureParent.style.width = '60vw';
+//   bloodPressure.style.width = '60vw';
+//   bloodPressure.style.flexDirection = 'row';
 
-  let bloodSugarParent = bloodSugar.parentElement;
-  bloodSugarParent.style.width = '20vw';
-  bloodSugar.style.width = '20vw';
-  bloodSugar.style.flexDirection = 'column';
+//   let bloodSugarParent = bloodSugar.parentElement;
+//   bloodSugarParent.style.width = '20vw';
+//   bloodSugar.style.width = '20vw';
+//   bloodSugar.style.flexDirection = 'column';
 
 
-}
-});
+// }
+// });
 
-bloodSugar.addEventListener("click", function() {
+// bloodSugar.addEventListener("click", function() {
+//   console.log('clicked');
+//   if(!bloodSugar.classList.contains('active')) {
+//   bloodSugar.classList.toggle('active');
+//   bloodPressure.classList.toggle('active');
+
+//   let bloodSugarParent = bloodSugar.parentElement;
+//   bloodSugarParent.style.width = '60vw';
+//   bloodSugar.style.width = '60vw';
+//   bloodSugar.style.flexDirection = 'row';
+
+
+//   let bloodPressureParent = bloodPressure.parentElement;
+//   bloodPressureParent.style.width = '20vw';
+//   bloodPressure.style.width = '20vw';
+//   bloodPressure.style.flexDirection = 'column';
+
+
+// }
+// });
+
+const bpButton = document.getElementById('bpNext');
+const bsButton = document.getElementById('bsNext');
+
+bpButton.addEventListener('click', function() {
   console.log('clicked');
-  if(!bloodSugar.classList.contains('active')) {
-  bloodSugar.classList.toggle('active');
-  bloodPressure.classList.toggle('active');
+  document.getElementById('bloodPressureParent').style.display = 'none';
+  document.getElementById('bloodSugarParent').style.display = 'flex';
+})
 
-  let bloodSugarParent = bloodSugar.parentElement;
-  bloodSugarParent.style.width = '60vw';
-  bloodSugar.style.width = '60vw';
-  bloodSugar.style.flexDirection = 'row';
-
-
-  let bloodPressureParent = bloodPressure.parentElement;
-  bloodPressureParent.style.width = '20vw';
-  bloodPressure.style.width = '20vw';
-  bloodPressure.style.flexDirection = 'column';
-
-
-}
-});
+bsButton.addEventListener('click', function() {
+  console.log('clicked');
+  document.getElementById('bloodSugarParent').style.display = 'none';
+  document.getElementById('bloodPressureParent').style.display = 'flex';
+})
